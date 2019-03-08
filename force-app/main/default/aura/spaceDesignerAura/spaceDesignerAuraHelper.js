55b9fa0b44c2ab684ab0af11bdd0e106aad07f0b
Kaygusuz <yakup.kaygusuz@accenture.com>
2019-03-08 14:20:30 +1100

    finished flow and flow parent helper

---
+++ force-app/main/default/aura/spaceDesignerAura/spaceDesignerAuraHelper.js
@@ -0,0 +1,16 @@
+({
+    startFlow : function(component, recId, market)
+    {
+        var flow = component.find("flowCmp");
+
+        var inputVariables = [
+            {name : "varReservId" , type : "String" , value : recId},
+            {name : "varMarketId", type: "String", value : market},
+        ];
+
+        if(flow)
+        {
+            flow.startFlow("spaceDesigner", inputVariables);
+        }
+    }
+})
